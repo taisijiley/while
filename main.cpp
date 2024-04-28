5596afb5a6dba5540cd178ff15a000998a1fe2d6
Teren <Official.Teren@yandex.ru>
2024-04-28 15:05:03 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// понятие цикла 
+// цикл while
+int main()
+{
+	/*
+	while (утвеждение)
+	{
+		действие для повторения
+	}
+	*/
+
+	int a = 0;
+	while (++a <= 10)
+	{
+		cout << a << endl;
+	}
+}
